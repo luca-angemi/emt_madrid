3502b3ad6f07da338fb0314459297c1de97dbe2a
Luca Angemi <luca.angemi@gmail.com>
2024-11-28 10:16:44 +0100

    Update sensor

--- custom_components/emt_madrid/sensor.py
+++ custom_components/emt_madrid/sensor.py
@@ -10,10 +10,12 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTime
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import slugify
 
 from .const import DOMAIN
-from .entity import AirFryerEntity
+from .coordinator import EMTCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,7 +36,7 @@ async def async_setup_entry(
     """Set up the EMT Madrid sensor."""
     coordinator = config_entry.runtime_data
     entities = [
-        EMTMadridSensor(config_entry, SENSOR_TYPE_EMT[0], coordinator, line, stop_id)
+        EMTMadridSensor(SENSOR_TYPE_EMT[0], coordinator, line, stop_id)
         for stop_id in list(coordinator.lines.keys())
         for line in coordinator.lines[stop_id]
     ]
@@ -42,25 +44,29 @@ async def async_setup_entry(
     async_add_entities(entities)
 
 
-class EMTMadridSensor(AirFryerEntity, SensorEntity):
+class EMTMadridSensor(CoordinatorEntity[EMTCoordinator], SensorEntity):
     """EMT Madrid Sensor."""
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
         description: SensorEntityDescription,
         coordinator,
         line,
         stop_id,
     ) -> None:
         """Initialize a EMT Madrid sensor."""
-        super().__init__(config_entry, description, coordinator)
+        super().__init__(coordinator)
         self.entity_description = description
         self.line = line
         self.stop_id = stop_id
-        self._attr_extra_state_attributes = {"stop_id": stop_id}
         self._attr_name = "Bus " + line + " Next Arrival"
         self._attr_unique_id = slugify(DOMAIN + " " + stop_id + " " + line,separator="_")
+        self._device_id = "emt_madrid_bus_stop_" + stop_id
+        self._attr_device_info = dr.DeviceInfo(
+            identifiers={(DOMAIN, self._device_id)},
+            entry_type=dr.DeviceEntryType.SERVICE,
+            name="Bus Stop " + stop_id,
+        )
 
     @property
     def native_value(self):
