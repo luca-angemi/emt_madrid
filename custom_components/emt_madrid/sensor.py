65e25b172ee8cae77e9fee8116a04efad4cf24df
Luca Angemi <luca.angemi@gmail.com>
2026-09-18 08:44:35 +0200

    Remove unused import from sensor.py
    Removed unused import of SensorStateClass.

--- custom_components/emt_madrid/sensor.py
+++ custom_components/emt_madrid/sensor.py
@@ -6,7 +6,6 @@ from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
-    SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTime
